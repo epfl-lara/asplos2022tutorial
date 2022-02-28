9a99ccff3e77d3138f2de103e8415688b28164e7
Viktor Kuncak <viktor.kuncak@epfl.ch>
2022-03-01 00:45:10 +0100

    binary search

---
+++ 03-q-binary-search/BinarySearch.scala
@@ -0,0 +1,13 @@
+object BinarySearch1Solution {
+  def search(arr: Array[Int], x: Int, lo: Int, hi: Int): Boolean = {
+    if (lo <= hi) {
+      val i = (lo + hi) / 2
+      val y = arr(i)
+      if (x == y) true
+      else if (x < y) search(arr, x, lo, i-1)
+      else search(arr, x, i+1, hi)
+    } else {
+      false
+    }
+  }
+}
